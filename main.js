f1381ec88a9afeb70df55640f523f66c384d0886
lasaqna111 <nknsyt1@gmail.com>
2024-08-07 11:54:01 +0900

    disable start_button while rotating

--- main.js
+++ main.js
@@ -7,26 +7,31 @@ let context=canvas.getContext('2d');
 const start_button=document.getElementById("start");
 const stop_button=document.getElementById("stop");
 
+
 // set global var
 let rotate_flag=false;
 
 
 
-
-function delay(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
-
-
-
-
 let reel=new Reel(5,canvas,context);
+stop_button.disabled=true;
 
 start_button.addEventListener('click',async ()=>{
-
-    window.requestAnimationFrame(reel.rotate_animate());
+    if(!start_button.disabled&&reel.flag==false){
+        rotate_flag=false;
+        
+    }
+    if(!rotate_flag){
+        rotate_flag=true;
+        start_button.disabled=true;
+        stop_button.disabled=false;
+    
+        reel.rotate_animate();
+    }
 });
 
 stop_button.addEventListener('click',() =>{
     reel.flag=true;
+    stop_button.disabled=true;
+    start_button.disabled=false;
 });
